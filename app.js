720120749b997e0470c8771b47bcbad15c7758f8
loud <loud@drore.com>
2018-05-29 10:29:17 +0800

    添加在线人员列表

--- app.js
+++ app.js
@@ -12,18 +12,60 @@ module.exports = app;*/
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var list = []
+var allUser = []
 //var router = require('express').Router()
 io.on('connection',function(socket){
+	//console.log(socket)
 	socket.on('join',function(data){
+		var user = {}
+		user.name = data;
+		user.id = socket.id;
+		allUser.push(user);
 		io.emit('serverJoin',data+'加入了聊天室')
+		io.emit('showList',allUser)
 	})
 	socket.on('messageSend',function(data){
 		//io.emit('serverSend',data.time+':'+data.value ) //发给所有人
 		socket.broadcast.emit('serverSend',data)  //发送给除了自己以外的所有人
 	})
-	socket.on('close',function(data){
+	socket.on('someMessage',function(data){
+		//console.log('群内发送')
+		for(var i=0;i<list.length;i++){
+			io.sockets.to(list[i]).emit('serverSend',data)
+		}
+		
+	})
+	socket.on('add',function(data){
+		list.push(socket.id)
+		io.emit('serverJoin',data+'加入了群聊')
+		console.log(list)
+	})
+
+	socket.on('leave',function(data){
+		var index = list.indexOf(socket.id)
+		list.splice(index, 1)
+		io.emit('serverJoin',data+'离开了群聊')
+	})
+
+	/*socket.on('close',function(data){
 		io.emit('serverJoin',data+'离开了聊天室')
-		socket.disconnect(true)	
+		socket.disconnect(true,111)		
+	})*/
+
+	socket.on('disconnect',function(){ //当浏览器刷新的时候会触发  socket.disconnect(true)也会触发
+		var index;
+		for (var i=0;i<allUser.length;i++){
+			if(socket.id == allUser[i].id){
+				index = i;
+				break;
+			}
+		}
+		var name = allUser[i].name
+		allUser.splice(index,1)
+		io.emit('showList',allUser)
+		io.emit('serverJoin',name+'离开了聊天室')
+		//socket.disconnect(true)	
 	})
 
 })
